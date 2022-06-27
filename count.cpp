cedd791e4cf7f24746d1c44ba24e00ea7761872a
dongxh <dongxh@outlook.com>
2022-06-27 23:56:30 +0800

    Changes

---
+++ count.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 董晓辉 on 2022/6/27.
+//
+#include "iostream"
+
+int main()
+{
+    int currVal = 0;
+    int val = 0;
+
+    if (std::cin >> currVal){
+        int cnt = 1;
+        while (std::cin>>val){
+            if(val == currVal)
+                cnt++;
+            else {
+                std::cout << currVal << "occurs " << cnt << "times" << std::endl;
+                currVal = val;
+                cnt = 1;
+            }
+        }
+        std::cout << currVal << "occurs " << cnt << "times" << std::endl;
+    }
+    return 0;
+}
---
+++ read_item.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 董晓辉 on 2022/6/27.
+//
+
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    Sales_item book;
+    std::cin >> book;
+    std::cout << book << std::endl;
+    return 0;
+}
